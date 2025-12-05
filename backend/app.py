b8b6cfcd03b480ca7048c4b83ebc67acdee4805b
俞青云 <505006159@qq.com>
2025-12-05 22:52:12 +0800

    1

--- backend/app.py
+++ backend/app.py
@@ -653,4 +653,4 @@ def serve_index():
     return send_from_directory(frontend_dist_path, 'index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5050)
